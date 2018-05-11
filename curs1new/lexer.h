d2450be2413bec363f693ffb56d5bec894902405
alex.bakharew <alex.bakharew@gmail.com>
2018-05-12 00:49:05 +0300

    General update

--- curs1new/lexer.h
+++ curs1new/lexer.h
@@ -3,51 +3,103 @@
 #define LEXER_H
 #include "baselexer.h"
 //********************************************
-//*        Developed by  xxx		     *
+//*        Developed by  BAT		     *
 //*             (c)  2018                    *
 //********************************************
 class tLexer:public tBaseLexer{
 public:
-// пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ
+// персональный код разработчика
  std::string Authentication()const{
                      return "BAT"
                 +std::string("2018");}
-//пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ
+//конструктор
  tLexer():tBaseLexer(){
-//пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ:
-
-//  пїЅпїЅпїЅпїЅ Azero
+//создать автоматы:
+//  ноль Azero
     addstr  (Azero,0,"+-", 2);
     addstr  (Azero,0,"0",  1);
     addstr  (Azero,2,"0",  1);
   Azero.final(1);
 
 //________________________________________
-
-// пїЅпїЅпїЅпїЅпїЅ
-    addstr  (Adec,0,"0",   3);
-    addstr  (Adec,3,".",   2);
-    addrange(Adec,0,'1','9',1);
-    addstr  (Adec,1,".",   2);
-    addrange(Adec,2,'0','9',2);
-    addrange(Adec,1,'7','8',1);
-  Adec.final(1);
-  Adec.final(2);
+// число
+//0
+  addrange(Adec,0,'1','9',1);
+  addstr(Adec,0,"+",2);
+  addstr(Adec,0,"-",2);
+  addstr(Adec,0,".",3);
+  addstr(Adec,0,"0",4);
+//1
+  addrange(Adec,1,'0','9',1);
+  addstr(Adec,1,"e",7);
+  addstr(Adec,1,"E",7);
+  addstr(Adec,1,".",6);
+//2
+  addstr(Adec,2,".",3);
+  addrange(Adec,2,'1','9',1);
+  addstr(Adec,2,"0",4);
+//3
+  addrange(Adec,3,'0','9',3);
+  addstr(Adec,3,"e",5);
+  addstr(Adec,3,"E",5);
+//4
+  addstr(Adec,4,".",3);
+  addstr(Adec,4,"e",5);
+  addstr(Adec,4,"E",5);
+//5
+  addstr(Adec,5,"+",9);
+  addstr(Adec,5,"-",9);
+//6
+  addrange(Adec,6,'0','9',6);
+  addstr(Adec,6,"e",7);
+  addstr(Adec,6,"E",7);
+//7
+  addstr(Adec,7,"+",8);
+  addstr(Adec,7,"-",8);
+//8
+  addrange(Adec,8,'0','9',10);
+//9
+  addrange(Adec,9,'0','9',9);
+//10
+  addrange(Adec,10,'0','9',10);
+//......................
+ Adec.final(1);
+ Adec.final(3);
+ Adec.final(6);
+ Adec.final(9);
+ Adec.final(10);
+///////////////////////
 //________________________________________
-
-// пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ
+// идентификатор
+//0
     addrange(Aid,0,'a','z',1);
+    addrange(Aid,0,'A','Z',1);
+    addstr(Aid,0,"!",1);
+    
+//1
     addrange(Aid,1,'a','z',1);
-    addstr  (Aid,1,"!-",   1);
+    addrange(Aid,1,'A','Z',1);
+    addrange(Aid,1,'0','9',1);
+    addstr(Aid,1,"-",1);
+    addstr(Aid,1,"!",1);
+//......................
   Aid.final(1);
 //________________________________________
-
-// пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ
-    addstr  (Aidq,0,"?",   1);
+// идентификатор предиката
+//0
     addrange(Aidq,0,'a','z',1);
+    addrange(Aidq,0,'A','Z',1);
+    addstr(Aidq,0,"-",1);
+    addstr(Aidq,0,"?",2);
+    
+//1
     addrange(Aidq,1,'a','z',1);
-    addstr  (Aidq,1,"?-",   1);
-  Aidq.final(1);
+    addrange(Aidq,1,'A','Z',1);
+    addrange(Aidq,1,'0','9',1);
+    addstr(Aidq,1,"-",1);
+    addstr(Aidq,1,"?",2);
+//2
+  Aidq.final(2);
 }
 };
 #endif
